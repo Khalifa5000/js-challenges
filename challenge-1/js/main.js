e20d6bc70084530ecf4f16a543c295a3b44e5a6b
khalifa5000 <m.khalifa5000@gmail.com>
2024-06-06 21:52:57 +0300

    Edit

--- challenge-1/js/main.js
+++ challenge-1/js/main.js
@@ -1,11 +1,13 @@
-let title = "Elzero", describtion = "Elzero web schoole", date="25 / 10";
+let userTitle = "Elzero",
+ userDescribtion = "Elzero web schoole",
+ registrationDate="25/10";
 
 let markUp = `
 <div class="card">
   <div class="child">
-  <h3> Hello ${title}</h3>
-  <p>${describtion}</p>
-  <span>${date}</span>
+  <h3> Hello ${userTitle}</h3>
+  <p>${userDescribtion}</p>
+  <span>${registrationDate}</span>
   </div>
 </div>
 `;
