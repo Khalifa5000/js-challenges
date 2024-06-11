d1a1a95792ad81cfee90c1b3e6cf34705fbf08aa
khalifa5000 <m.khalifa5000@gmail.com>
2024-06-11 18:27:51 +0300

    Edit

---
+++ challenge-video-22/js/main.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+// challenge 2
+
+let d = "-100";
+let e = "20";
+let f = 30;
+let g = true;
+
+console.log(-d * +e); //2000
+
+console.log(-d * f /  e + +e + g + g + g); //173
+
+// another answer
+console.log(-d + ++e * ++g + ++f); //173
+
