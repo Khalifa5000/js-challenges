a61aa47153c986315297816ffd20147eaf75e988
khalifa5000 <m.khalifa5000@gmail.com>
2024-06-11 18:38:56 +0300

    Edit

--- challenge-video-22/js/main.js
+++ challenge-video-22/js/main.js
@@ -13,8 +13,6 @@ let g = true;
 
 console.log(-d * +e); //2000
 
-console.log(-d * f /  e + +e + g + g + g); //173
 
-// another answer
-console.log(-d + ++e * ++g + ++f); //173
+console.log(++e * ++g  +  ++f + -d  ); //173
 
